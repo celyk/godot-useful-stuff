13ec5a1a75a89e1f1ac54aa9c31fcb3216b234ed
celyk <50609684+celyk@users.noreply.github.com>
2023-04-11 20:38:38 +1000

    Added documentation to ShaderTextEdit

--- scripts/ShaderTextEdit.gd
+++ scripts/ShaderTextEdit.gd
@@ -1,9 +1,11 @@
-# TextEdit box for editing a shader at runtime
+## A TextEdit box for editing a shader at runtime.
 
 class_name ShaderTextEdit extends TextEdit
 
+## The shader which you want to edit.
 @export var target_shader : Shader
 
+## Sets the code of the shader to the current text.
 func recompile():
 	if target_shader:
 		target_shader.code = text
