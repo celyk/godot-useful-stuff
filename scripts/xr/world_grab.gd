aa749f538fefa5da5df574f60aad7aff19ccedeb
celyk <50609684+celyk@users.noreply.github.com>
2023-09-12 03:29:21 +1000

    Bugfix WorldGrab

--- scripts/xr/world_grab.gd
+++ scripts/xr/world_grab.gd
@@ -16,7 +16,7 @@ class_name WorldGrab extends RefCounted
 
 ## The transform that takes one to the other. Intended for a one handed grab.
 func get_grab_transform(from : Transform3D, to : Transform3D) -> Transform3D:
-	return from.affine_inverse() * to
+	return to * from.affine_inverse()
 
 ## For orbitting around a central point, without scale, like spinning a globe.
 func get_orbit_transform(from_pivot : Vector3, from_b : Vector3, to_pivot : Vector3, to_b : Vector3) -> Transform3D:
@@ -26,44 +26,46 @@ func get_orbit_transform(from_pivot : Vector3, from_b : Vector3, to_pivot : Vect
 	
 	# Gather information on the shortest rotation
 	var axis : Vector3 = from_b.cross(to_b)
+	if axis == Vector3(): axis = Vector3.RIGHT
 	var angle : float = from_b.angle_to(to_b)
 
 	# Construct the transformation that orbits about the pivot, with no scale!
-	return Transform3D(Basis(axis, angle), to_pivot)
+	return Transform3D().translated(-from_pivot).rotated(axis.normalized(), angle).translated(to_pivot)
 
 ## This is a transformation which takes line (from_a,from_b) to line (to_a,to_b). It is analagous to pinch gesture on a touch screen.
 func get_pinch_transform(from_a : Vector3, from_b : Vector3, to_a : Vector3, to_b : Vector3) -> Transform3D:
-	var delta_scale : float = sqrt(to_b.dot(to_b) / from_b.dot(from_b))
+	var delta_scale : float = sqrt((to_b-to_a).length_squared() / (from_b-from_a).length_squared())
 	
 	# Orbit around pivot point a, and scale so that b is fixed in place.
 	# According to symmetry, it is the same as if a and b are swapped.
-	return get_orbit_transform(from_a, from_b, to_a, to_b) * Transform3D(Basis.from_scale(Vector3(1,1,1) * delta_scale))
+	return get_orbit_transform(from_a, from_b, to_a, to_b).translated(-to_a).scaled(Vector3.ONE * delta_scale).translated(to_a)
 
 ## Separable blending of position, rotation and scale. Fine tune smoothing for maximum comfort.
-## [br]
-## [br]Weights of 0 are optimized to avoid unnecessary compute.
 func split_blend(
 		from : Transform3D,
 		to : Transform3D, 
 		pos_weight : float = 0.0, 
 		rot_weight : float = 0.0, 
-		scale_weight : float = 0.0) -> Transform3D:
+		scale_weight : float = 0.0,
+		from_pivot : Vector3 = Vector3(),
+		to_pivot : Vector3 = Vector3()) -> Transform3D:
 	
-	# Interpolate position
-	if pos_weight != 0.0:
-		from.origin = from.origin.lerp(to.origin, pos_weight)
+	var src_scale : Vector3 = from.basis.get_scale()
+	var src_rot : Quaternion = from.basis.get_rotation_quaternion()
 	
-	# Interpolate rotation
-	var from_rot : Quaternion = from.basis.get_rotation_quaternion()
-	from.basis = Basis(from_rot)
-	if rot_weight != 0.0:
-		var to_rot : Quaternion = to.basis.get_rotation_quaternion()
-		from.basis = Basis(from_rot.slerp(to_rot, rot_weight))
+	var dst_scale : Vector3 = to.basis.get_scale()
+	var dst_rot : Quaternion = to.basis.get_rotation_quaternion()
 	
-	# Interpolate scale
-	if scale_weight != 0.0:
-		var from_scale : Vector3 = from.basis.get_scale()
-		var to_scale : Vector3 = to.basis.get_scale()
-		from.basis *= Basis.from_scale(from_scale.lerp(to_scale, scale_weight))
+	var basis_inv : Basis = from.basis.inverse()
+	from.basis = Basis(src_rot.slerp(dst_rot, rot_weight).normalized()) * Basis.from_scale(src_scale.lerp(dst_scale, scale_weight))
+	
+	#from.origin -= from_pivot
+	#to.origin -= to_pivot
+	from.origin = from.origin.lerp(to.origin, pos_weight)
+	#from.origin = from_pivot.lerp(to_pivot, pos_weight) + from.origin.slerp(to.origin, pos_weight)
+
+	#from.origin -= from_pivot
+	#from.origin = from.basis * (basis_inv * from.origin)
+	#from.origin += from_pivot.lerp(to_pivot, pos_weight)
 	
 	return from
