c3451b2b9c62720775d71b9b6ff3b2c565baebcf
celyk <50609684+celyk@users.noreply.github.com>
2023-10-01 12:59:30 +1100

    Proper cleanup and comments

--- scripts/hello_triangle.gd
+++ scripts/hello_triangle.gd
@@ -2,13 +2,16 @@
 class_name HelloTriangleEffect extends RenderingEffect
 
 # TODO
-# Proper cleanup
 # Find a way to get internal framebuffer
-# Add documentation and comments
+# Add documentation
 # XR support
 # Fix hardcoded _framebuffer_format
 
+
+# PUBLIC
+
 var transform : Transform3D
+
 
 # PRIVATE
 
@@ -18,13 +21,13 @@ var _framebuffer_format
 
 var _p_render_pipeline : RID
 var _p_render_pipeline_uniform_set : RID
+var p_vertex_buffer : RID
 var _p_vertex_array : RID
 var _p_shader : RID
 var _clear_colors := PackedColorArray([Color.DARK_BLUE])
 
 func _init():
 	_RD = RenderingServer.get_rendering_device()
-	
 	RenderingServer.call_on_render_thread(_initialize_render)
 
 func _compile_shader(source_fragment : String = _default_source_fragment, source_vertex : String = _default_source_vertex) -> RID:
@@ -44,48 +47,45 @@ func _compile_shader(source_fragment : String = _default_source_fragment, source
 	return p_shader
 
 func _initialize_render():
-	#_p_framebuffer = _RD.framebuffer_create_empty(Vector2i(100,100))
-	#_framebuffer_format = _RD.screen_get_framebuffer_format()
+	# Get the framebuffer format, somehow
 	var attachment_formats = [RDAttachmentFormat.new(),RDAttachmentFormat.new()]
-	attachment_formats[0].format = RenderingDevice.DATA_FORMAT_R16G16B16A16_SFLOAT
 	attachment_formats[0].usage_flags = _RD.TEXTURE_USAGE_COLOR_ATTACHMENT_BIT
-	attachment_formats[1].format = _RD.DATA_FORMAT_D24_UNORM_S8_UINT
+	attachment_formats[0].format = RenderingDevice.DATA_FORMAT_R16G16B16A16_SFLOAT
 	attachment_formats[1].usage_flags = _RD.TEXTURE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT
+	attachment_formats[1].format = _RD.DATA_FORMAT_D24_UNORM_S8_UINT if _RD.texture_is_format_supported_for_usage(_RD.DATA_FORMAT_D24_UNORM_S8_UINT, attachment_formats[1].usage_flags) else _RD.DATA_FORMAT_D32_SFLOAT_S8_UINT
 	
 	_framebuffer_format = _RD.framebuffer_format_create( attachment_formats )
 	
+	
 	_p_shader = _compile_shader()
 	
-	
+	# Create vertex buffer
 	var vertex_buffer_bytes : PackedByteArray = _vertex_buffer.to_byte_array()
-	var p_vertex_buffer : RID = _RD.vertex_buffer_create(vertex_buffer_bytes.size(), vertex_buffer_bytes)
+	p_vertex_buffer = _RD.vertex_buffer_create(vertex_buffer_bytes.size(), vertex_buffer_bytes)
 	
+	# A little trick to reuse the same buffer for multiple attributes
 	var vertex_buffers := [p_vertex_buffer, p_vertex_buffer]
 	
-	var sizeof_float := 4
-	var stride := 7
+	var sizeof_float := 4 # Needed to compute byte offset and stride
+	var stride := 7 # How far until the next element
 	
 	var vertex_attrs = [RDVertexAttribute.new(), RDVertexAttribute.new()]
-	vertex_attrs[0].format = _RD.DATA_FORMAT_R32G32B32_SFLOAT
-	vertex_attrs[0].location = 0
+	vertex_attrs[0].format = _RD.DATA_FORMAT_R32G32B32_SFLOAT # vec3 equivalent
+	vertex_attrs[0].location = 0 # layout binding
 	vertex_attrs[0].offset = 0 * sizeof_float
-	vertex_attrs[0].stride = stride * sizeof_float
-	vertex_attrs[1].format = _RD.DATA_FORMAT_R32G32B32A32_SFLOAT
-	vertex_attrs[1].location = 1
+	vertex_attrs[0].stride = stride * sizeof_float # How far until the next element, in bytes
+	vertex_attrs[1].format = _RD.DATA_FORMAT_R32G32B32A32_SFLOAT # vec4 equivalent
+	vertex_attrs[1].location = 1  # layout binding
 	vertex_attrs[1].offset = 3 * sizeof_float
-	vertex_attrs[1].stride = stride * sizeof_float
+	vertex_attrs[1].stride = stride * sizeof_float # How far until the next element, in bytes
 	var vertex_format = _RD.vertex_format_create(vertex_attrs)
 	
+	# Create a VAO, which keeps all of our vertex state handy for later
 	_p_vertex_array = _RD.vertex_array_create(_vertex_buffer.size()/stride, vertex_format, vertex_buffers)
 	
-	#var uniforms = [RDUniform.new()]
-	#uniforms[0].binding = 0
-	#uniforms[0].uniform_type = _RD.UNIFORM_TYPE_UNIFORM_BUFFER
-	#uniforms[0].push_back( uniform )
-	
-	#_p_render_pipeline_uniform_set = _RD.uniform_set_create(uniforms, _p_shader, 0)
-	
+	# Inform the rasterizer what we need to do
 	var raster_state = RDPipelineRasterizationState.new()
+	raster_state.cull_mode = RenderingDevice.POLYGON_CULL_DISABLED
 	var depth_state = RDPipelineDepthStencilState.new()
 	depth_state.enable_depth_write = true
 	depth_state.enable_depth_test = true
@@ -94,6 +94,7 @@ func _initialize_render():
 	var blend = RDPipelineColorBlendState.new()
 	blend.attachments.push_back( RDPipelineColorBlendStateAttachment.new() )
 	
+	# Finally, create the render pipeline
 	_p_render_pipeline = _RD.render_pipeline_create(
 		_p_shader,
 		_framebuffer_format,
@@ -104,7 +105,6 @@ func _initialize_render():
 		depth_state,
 		blend)
 
-#var textures = [RID(), RID()]
 var _prev_size : Vector2i
 func _render_callback(_effect_callback_type : int, render_data : RenderData):
 	if _effect_callback_type != effect_callback_type: return
@@ -116,39 +116,24 @@ func _render_callback(_effect_callback_type : int, render_data : RenderData):
 	
 	var view_count : int = render_scene_buffers.get_view_count()
 	var current_size : Vector2i = render_scene_buffers.get_internal_size()
-	
-	if not _p_framebuffer.is_valid() || current_size != _prev_size:
-		print("framebuffer recreated")
+
+	# Recreate framebuffer if needed
+	if current_size != _prev_size:
+		# Unsure what to do here, just getting a lot of errors
+		#if _p_framebuffer.is_valid():
+		#	_RD.free_rid(_p_framebuffer)
+		
 		var textures = [ render_scene_buffers.get_color_texture(), render_scene_buffers.get_depth_texture() ]
 		_p_framebuffer = _RD.framebuffer_create(textures, -1, view_count)
 		
 		_prev_size = current_size
 	
-	#return
 	
-	#var textures := [render_scene_buffers.get_color_texture(), render_scene_buffers.get_depth_texture()]
-	#var p := RDFramebufferPass.new()
-	#p.color_attachments.push_back(0)
-	#p.depth_attachment = 1
-	#var passes := [p]
-	#_p_framebuffer = FramebufferCacheRD.get_cache_multipass(textures, passes, view_count) #_RD.framebuffer_create_empty(Vector2i(100,100))
+	_RD.draw_command_begin_label("Hello, Triangle!", Color(1.0, 1.0, 1.0, 1.0))
 	
 	# Loop through views just in case we're doing stereo rendering. No extra cost if this is mono.
 	for view in range(view_count):
-		# Get the RID for our color image, we will be reading from and writing to it.
-		#var _p_framebuffer : RID = render_scene_buffers.get_color_layer(view)
-		#var textures := [ render_scene_buffers.get_color_layer(view), render_scene_buffers.get_depth_layer(view) ]
-		#var p := RDFramebufferPass.new()
-		#p.color_attachments.push_back(0)
-		#p.depth_attachment = 1
 		
-		#var passes := [p]
-		#_p_framebuffer = FramebufferCacheRD.get_cache_multipass(textures, passes, view_count)
-		
-		#print(_p_framebuffer.is_valid())
-		#print(_p_framebuffer)
-		
-		#var draw_list : int = _RD.draw_list_begin_for_screen(DisplayServer.MAIN_WINDOW_ID, _clear_colors[0])
 		var draw_list : int = _RD.draw_list_begin(
 			_p_framebuffer, 
 			_RD.INITIAL_ACTION_CONTINUE,
@@ -159,11 +144,12 @@ func _render_callback(_effect_callback_type : int, render_data : RenderData):
 		
 		_RD.draw_list_bind_render_pipeline(draw_list, _p_render_pipeline)
 		_RD.draw_list_bind_vertex_array(draw_list, _p_vertex_array)
-		#_RD.draw_list_bind_uniform_set(draw_list, _p_render_pipeline_uniform_set, 0)
 		
+		# Setup model view projection
 		var MVP : Projection = Projection.create_depth_correction(true) * render_scene_data.get_view_projection(view)
 		MVP *= Projection(render_scene_data.get_cam_transform().inverse() * transform)
 		
+		# Send data to our shader
 		var buffer := PackedFloat32Array()
 		buffer.resize(16)
 		
@@ -173,14 +159,27 @@ func _render_callback(_effect_callback_type : int, render_data : RenderData):
 		var buffer_bytes : PackedByteArray = buffer.to_byte_array()
 		_RD.draw_list_set_push_constant(draw_list, buffer_bytes, buffer_bytes.size())
 		
+		# Draw it!
 		_RD.draw_list_draw(draw_list, false, 1)
 		
 		_RD.draw_list_end()
+	
+	_RD.draw_command_end_label()
 
 func _notification(what):
-	if what == NOTIFICATION_PREDELETE:
+	if what == NOTIFICATION_PREDELETE: # Cleanup
+		if _p_render_pipeline.is_valid():
+			_RD.free_rid(_p_render_pipeline)
 		if _p_shader.is_valid():
 			_RD.free_rid(_p_shader)
+		if _p_vertex_array.is_valid():
+			_RD.free_rid(_p_vertex_array)
+		if p_vertex_buffer.is_valid():
+			_RD.free_rid(p_vertex_buffer)
+		if _p_render_pipeline_uniform_set.is_valid():
+			_RD.free_rid(_p_render_pipeline_uniform_set)
+		if _p_framebuffer.is_valid():
+			_RD.free_rid(_p_framebuffer)
 
 var _vertex_buffer := PackedFloat32Array([
 		-0.3,-0.5,0.0, 1,0,0,1,
