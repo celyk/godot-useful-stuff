5e3e97ca81d93d1229ac113900324afd0842d284
celyk <humanimations10@gmail.com>
2024-10-07 20:31:40 +1100

    Remove fluff

--- addons/VertexHandles/VertexHandlesGizmo.gd
+++ addons/VertexHandles/VertexHandlesGizmo.gd
@@ -1,13 +1,9 @@
 @tool
 extends EditorNode3DGizmoPlugin
 
-#var mattest = load("res://new_shader_material.tres")
-
 func _init():
-	#print("init")
 	create_material("main", Color(0,1,1),false,true)
 	create_handle_material("handles",false)
-	
 
 const MyCustomNode3D = preload("VertexHandles.gd")
 func _has_gizmo(node):
@@ -39,14 +35,9 @@ func _get_handle_value(gizmo,id,secondary):
 func _set_handle(gizmo,id,secondary,camera,point):
 	var node3d : Node3D = gizmo.get_node_3d()
 	
-	
-	#ResourceSaver.save(get_material("handles",gizmo),"res://test_save.tres")
-	
 	var gt : Transform3D = node3d.get_global_transform()
 	var gi : Transform3D = gt.affine_inverse()
 	
-	#print(camera)
-
 	var ray_from : Vector3 = camera.project_ray_origin(point)
 	var ray_dir : Vector3 = camera.project_ray_normal(point)
 	
@@ -58,28 +49,12 @@ func _set_handle(gizmo,id,secondary,camera,point):
 	
 	if p.is_empty():
 		return
-		
+	
 	p = p[0]
-	
-	#var sg = [ gi * ray_from, gi * (ray_from + ray_dir * 16384) ]
-	
-	#print("Set")
 	
 	var d = p.distance_to(node3d.global_position)
 	
 	node3d.points[0][id] = p
-	
-	'''match id:
-		0:
-			if(d < 0.001):
-				d = 0.001
-
-			node3d.radius = d
-		1:
-			if(d < 0.001):
-				d = 0.001
-				
-			node3d.width = d - node3d.radius'''
 	
 	_redraw(gizmo)
 
@@ -115,17 +90,7 @@ func _redraw(gizmo):
 			handles.push_back( point_array[j] )
 			#print(arrays[Mesh.ARRAY_VERTEX][j])
 	
-	#handles.push_back( Vector3(node3d.radius,0,0) )
-	#handles.push_back( Vector3(node3d.radius + node3d.width,0,0) )
-	
 	#gizmo.add_lines(lines, get_material("main", gizmo), false)
 	
 	var billboard := false
 	gizmo.add_handles(handles, get_material("handles", gizmo), [], billboard)
-	
-	#print(mattest)
-	#gizmo.add_handles(handles, mattest, [], true)
-
-'''func _get_subgizmo_transform(gizmo, i:int) -> Transform3D:
-	var node3d : Node3D = gizmo.get_node_3d()
-	return Transform3D()'''
